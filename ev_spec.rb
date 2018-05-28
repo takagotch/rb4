69720ebbe2771546d979a38af1f1839a07ba82cc
takagotch <30462722+takagotch@users.noreply.github.com>
2018-05-29 04:45:12 +0900

    Update ev_spec.rb

--- ev_spec.rb
+++ ev_spec.rb
@@ -4,3 +4,10 @@ spec.each do |event|
   # Order$1 stream read backwards
 end
 
+specify do
+  expect { client.read_events_backward('some_stream')}.to output(<<~EOS).to_stderr
+    RubyEventStore::Client#read_events_backward has been deprecated.
+    Use following fluent API to receive exact results:
+    client.read.stream(stream_name).limit(count).from(start).backward.each.to_a
+  EOS
+end
